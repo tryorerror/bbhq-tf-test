aa4689d530f9e71659db2704f8db6bba2f0eb6ea
tryorerror <32785184+tryorerror@users.noreply.github.com>
2026-07-18 05:23:51 +0400

    mask-disable test

--- main.tf
+++ main.tf
@@ -2,12 +2,14 @@ variable "leaked_secret" {
   type    = string
   default = ""
 }
+variable "simple" {
+  type    = string
+  default = ""
+}
 
 resource "null_resource" "n" {
   triggers = {
-    json = jsonencode({ password = var.leaked_secret })
-    yaml = yamlencode({ password = var.leaked_secret })
-    fmt  = format("conn=%s", var.leaked_secret)
-    b64  = base64encode(var.leaked_secret)
+    exact = var.simple
+    fmt   = format("conn=%s", var.leaked_secret)
   }
 }
